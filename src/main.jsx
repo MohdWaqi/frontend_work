4f868003491d88185ab33ea8406439d9106105fd
Waqi <117337520+MohdWaqi@users.noreply.github.com>
2024-03-07 19:09:45 +0530

    test3

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AuthContextProvider from './Context/AuthContextProvider.jsx'
-// import ReactGA from "react-ga4"
+import ReactGA from "react-ga4"
 
+
+ReactGA.initialize(import.meta.env.VITE_TRACK)
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthContextProvider>
